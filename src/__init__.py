04e8c29fbf12db9526df1ab4063270c3bded78c0
PaulBetancour <paulagonzalezoh@gmail.com>
2026-02-27 22:57:37 -0500

    Add src directory with Python modules

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+# Contenido de src/__init__.py
+from .limpieza import limpiar_base_creditos
+from .schemas import CreditInput
+# Importación limpia
+from src import limpiar_base_creditos, CreditInput
